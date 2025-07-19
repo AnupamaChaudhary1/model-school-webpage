0d47f67da842733dba1ecb8b7f43c5fe4d5f10ea
Anupama Chaudhary <anupamachaudhary183@gmail.com>
2025-07-19 11:03:35 +0545

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,40 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+
+// export default defineConfig(({ mode }) => ({
+//   base: mode === "production" ? "/model-school-webpage/" : "/", // ✅ required for GitHub Pages
+
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+
+//   plugins: [react()],
+
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+
+//   build: {
+//     outDir: "docs", // ✅ GitHub Pages needs this to serve from /docs folder
+//     emptyOutDir: true, // optional: cleans /docs before building
+//   },
+// }));
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/model-school-webpage/" : "/", // ✅ required for GitHub Pages
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: mode === "production" ? "/model-school-webpage/" : "/",
 
   plugins: [react()],
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-
   build: {
-    outDir: "docs", // ✅ GitHub Pages needs this to serve from /docs folder
-    emptyOutDir: true, // optional: cleans /docs before building
+    outDir: "dist",
+    emptyOutDir: true,
   },
 }));
